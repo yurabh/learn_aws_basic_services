c6e71190a35d7dba9c3eb6cdaf3648eea7953e45
yurabh <yurabh1990@gmail.com>
2022-12-06 16:58:22 +0200

    Start working with AWS and s3 bucket.

---
+++ src/main/java/com/learn/aws/basic/services/LearnAwsBasicServicesApplication.java
@@ -0,0 +1,12 @@
+package com.learn.aws.basic.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAwsBasicServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnAwsBasicServicesApplication.class, args);
+    }
+}
---
+++ src/main/java/com/learn/aws/basic/services/s3/S3Bucket.java
@@ -0,0 +1,49 @@
+package com.learn.aws.basic.services.s3;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+
+import java.io.File;
+import java.io.InputStream;
+
+public class S3Bucket {
+    private static final String REGION = "us-east-1";
+    private static final String bucketName = "number-phone-bucket";
+    private static final String key = "phones/phones-lists.txt";
+    private static final String accessKey = "AKIA4PJP54IWOMHBVZS7";
+    private static final String secretKey = "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";
+    private static final AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+    private static final File file = new File("C:\\Users\\Yuriy_Bahlay\\IdeaProjects\\learn_aws_basic_services\\phones-lists.txt");
+    private static final AmazonS3 amazonS3 = AmazonS3ClientBuilder
+            .standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withRegion(REGION)
+            .build();
+
+    private static boolean checkIfS3BucketExists() {
+        return amazonS3.doesBucketExist(bucketName);
+    }
+
+    private static boolean checkIfObjectExists() {
+        return amazonS3.doesObjectExist(bucketName, key);
+    }
+
+    private static void getContentFromFileByBucketNameAndKey() {
+        GetObjectRequest objectRequest = new GetObjectRequest(bucketName, key);
+        if (checkIfS3BucketExists() && checkIfObjectExists()) {
+            amazonS3.getObject(objectRequest, file);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        getContentFromFileByBucketNameAndKey();
+        S3Object s3Object = amazonS3.getObject(bucketName, key);
+        InputStream inputStream = s3Object.getObjectContent();
+    }
+}
