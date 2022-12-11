9dc28233c73017f2e7e7dbce19687250e69ffad3
yurabh <yurabh1990@gmail.com>
2022-12-11 22:51:10 +0200

    Refactoring code in S3 bucket class. And add dependency for Rds Aws.

--- src/main/java/com/learn/aws/basic/services/s3/S3Bucket.java
+++ src/main/java/com/learn/aws/basic/services/s3/S3Bucket.java
@@ -1,49 +1,46 @@
 package com.learn.aws.basic.services.s3;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
 
-import java.io.File;
-import java.io.InputStream;
+import static com.learn.aws.basic.services.AwsUtils.CREDENTIALS;
+import static com.learn.aws.basic.services.AwsUtils.DESTINATION_BUCKET_NAME;
+import static com.learn.aws.basic.services.AwsUtils.FILE;
+import static com.learn.aws.basic.services.AwsUtils.REGION;
+import static com.learn.aws.basic.services.AwsUtils.SOURCE_BUCKET_NAME;
+import static com.learn.aws.basic.services.AwsUtils.SOURCE_KEY;
 
 public class S3Bucket {
-    private static final String REGION = "us-east-1";
-    private static final String bucketName = "number-phone-bucket";
-    private static final String key = "phones/phones-lists.txt";
-    private static final String accessKey = "";
-    private static final String secretKey = "";
-    private static final AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
-    private static final File file = new File("");
+
     private static final AmazonS3 amazonS3 = AmazonS3ClientBuilder
             .standard()
-            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withCredentials(new AWSStaticCredentialsProvider(CREDENTIALS))
             .withRegion(REGION)
             .build();
 
     private static boolean checkIfS3BucketExists() {
-        return amazonS3.doesBucketExist(bucketName);
+        return amazonS3.doesBucketExist(SOURCE_BUCKET_NAME);
     }
 
     private static boolean checkIfObjectExists() {
-        return amazonS3.doesObjectExist(bucketName, key);
+        return amazonS3.doesObjectExist(SOURCE_BUCKET_NAME, SOURCE_KEY);
     }
 
     private static void getContentFromFileByBucketNameAndKey() {
-        GetObjectRequest objectRequest = new GetObjectRequest(bucketName, key);
+        GetObjectRequest objectRequest = new GetObjectRequest(SOURCE_BUCKET_NAME, SOURCE_KEY);
         if (checkIfS3BucketExists() && checkIfObjectExists()) {
-            amazonS3.getObject(objectRequest, file);
-
+            amazonS3.getObject(objectRequest, FILE);
         }
+    }
+
+    private static void moveObjectFromSourceBucketToDestinationBucket() {
+        amazonS3.copyObject(SOURCE_BUCKET_NAME, SOURCE_KEY, DESTINATION_BUCKET_NAME, SOURCE_KEY);
     }
 
     public static void main(String[] args) {
         getContentFromFileByBucketNameAndKey();
-        S3Object s3Object = amazonS3.getObject(bucketName, key);
-        InputStream inputStream = s3Object.getObjectContent();
+        moveObjectFromSourceBucketToDestinationBucket();
     }
 }
