7f9736f837762b0f931bfa1fd54f7b599ad0f415
yurabh <yurabh1990@gmail.com>
2022-12-24 16:50:00 +0200

    Add dependency for sqs aws and also basic learning sqs service.

--- src/main/java/com/learn/aws/basic/services/LearnAwsBasicServicesApplication.java
+++ src/main/java/com/learn/aws/basic/services/LearnAwsBasicServicesApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LearnAwsBasicServicesApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LearnAwsBasicServicesApplication.class, args);
     }
---
+++ src/main/java/com/learn/aws/basic/services/sqs/Sqs.java
@@ -0,0 +1,45 @@
+package com.learn.aws.basic.services.sqs;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.GetQueueUrlResult;
+import com.amazonaws.services.sqs.model.ReceiveMessageResult;
+
+import static com.learn.aws.basic.services.utils.AwsUtils.CREDENTIALS;
+import static com.learn.aws.basic.services.utils.AwsUtils.REGION;
+
+public class Sqs {
+    private static final AmazonSQS amazonSQS = AmazonSQSClientBuilder
+            .standard()
+            .withCredentials(new AWSStaticCredentialsProvider(CREDENTIALS))
+            .withRegion(REGION)
+            .build();
+
+    private static final String QUEUE = "myQueue";
+
+    private static void createQueue() {
+        amazonSQS.createQueue(QUEUE);
+    }
+
+    private static void sendMessageToTheQueue() {
+        GetQueueUrlResult queueUrl = amazonSQS.getQueueUrl(QUEUE);
+        amazonSQS.sendMessage(queueUrl.getQueueUrl(), "This is basic message sent to the queue");
+    }
+
+    private static void receiveMessageFromQueue() {
+        GetQueueUrlResult urlResult = amazonSQS.getQueueUrl(QUEUE);
+        ReceiveMessageResult receiveMessageResult = amazonSQS.receiveMessage(urlResult.getQueueUrl());
+        receiveMessageResult.getMessages().forEach(message -> {
+            System.out.println("Queue messageId: " + message.getMessageId());
+            System.out.println("Queue message body:  " + message.getBody());
+            System.out.println("Remove message: " + amazonSQS.deleteMessage(urlResult.getQueueUrl(), message.getReceiptHandle()));
+        });
+    }
+
+    public static void main(String[] args) {
+        createQueue();
+        sendMessageToTheQueue();
+        receiveMessageFromQueue();
+    }
+}
